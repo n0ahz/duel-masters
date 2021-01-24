5f365d266bd959f1d7276d3901e9456101570ef0
Md. Hasan-Uz-Zaman Rashid <hz.ce06@gmail.com>
2021-01-24 17:59:31 +0600

    MOD: phaser related module game-engine created and test component scene moved to separate class of its own..this is how the structure should be henceforth.

---
+++ dm-frontend/src/app/game-engine/scenes/test.scene.ts
@@ -0,0 +1,59 @@
+import * as Phaser from "phaser";
+
+export class TestScene extends Phaser.Scene {
+  green: any;
+  blue: any;
+  bg: any;
+  greenKeys: any;
+  blueKeys: any;
+
+  constructor() {
+    super({ key: 'Test' });
+  }
+
+  preload() {
+    this.load.setBaseURL('');
+    this.load.image('bg', 'assets/imgs/bg.jpg');
+    // this.add.image(1280/2, 960/2, 'bg');
+    // this.load.setBaseURL('http://labs.phaser.io');
+    // this.load.image('blueBox', 'assets/tilemaps/tiles/ground-tile.png');
+    // this.load.image('greenBox', 'assets/tilemaps/tiles/ground-tile.png');
+  }
+
+  create() {
+    this.bg = this.add.image(window.innerWidth/2, window.innerHeight/2, 'bg').setAlpha(0.5,0.5,0.5,0.5);
+    this.blue = this.physics.add.image(100, 100, 'blueBox').setCollideWorldBounds(true);
+    this.green = this.physics.add.image(300, 340, 'greenBox').setCollideWorldBounds(true);
+
+    this.greenKeys = this.input.keyboard.createCursorKeys();
+    this.blueKeys = this.input.keyboard.addKeys('a,s,d,w');
+
+    this.physics.add.collider(this.green, this.blue, null);
+  }
+
+  update() {
+    if (this.blueKeys.a.isDown) {
+      this.blue.setVelocityX(-200);
+    } else if (this.blueKeys.d.isDown) {
+      this.blue.setVelocityX(200);
+    } else if (this.blueKeys.w.isDown) {
+      this.blue.setVelocityY(-200);
+    } else if (this.blueKeys.s.isDown) {
+      this.blue.setVelocityY(200);
+    } else if (!this.blue.triggered) {
+      this.blue.setVelocity(0);
+    }
+
+    if (this.greenKeys.left.isDown) {
+      this.green.setVelocityX(-200);
+    } else if (this.greenKeys.right.isDown) {
+      this.green.setVelocityX(200);
+    } else if (this.greenKeys.up.isDown) {
+      this.green.setVelocityY(-200);
+    } else if (this.greenKeys.down.isDown) {
+      this.green.setVelocityY(200);
+    } else if (!this.green.triggered) {
+      this.green.setVelocity(0);
+    }
+  }
+}
--- dm-frontend/src/app/phaser-test/phaser-test.component.ts
+++ dm-frontend/src/app/phaser-test/phaser-test.component.ts
@@ -1,63 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as Phaser from 'phaser';
-
-class NewScene extends Phaser.Scene {
-  green: any;
-  blue: any;
-  bg: any;
-  greenKeys: any;
-  blueKeys: any;
-
-  constructor() {
-    super({ key: 'Test' });
-  }
-
-  preload() {
-    this.load.setBaseURL('');
-    this.load.image('bg', 'assets/imgs/bg.jpg');
-    // this.add.image(1280/2, 960/2, 'bg');
-    // this.load.setBaseURL('http://labs.phaser.io');
-    // this.load.image('blueBox', 'assets/tilemaps/tiles/ground-tile.png');
-    // this.load.image('greenBox', 'assets/tilemaps/tiles/ground-tile.png');
-  }
-
-  create() {
-    this.bg = this.add.image(window.innerWidth/2, window.innerHeight/2, 'bg').setAlpha(0.5,0.5,0.5,0.5);
-    this.blue = this.physics.add.image(100, 100, 'blueBox').setCollideWorldBounds(true);
-    this.green = this.physics.add.image(300, 340, 'greenBox').setCollideWorldBounds(true);
-
-    this.greenKeys = this.input.keyboard.createCursorKeys();
-    this.blueKeys = this.input.keyboard.addKeys('a,s,d,w');
-
-    this.physics.add.collider(this.green, this.blue, null);
-  }
-
-  update() {
-    if (this.blueKeys.a.isDown) {
-      this.blue.setVelocityX(-200);
-    } else if (this.blueKeys.d.isDown) {
-      this.blue.setVelocityX(200);
-    } else if (this.blueKeys.w.isDown) {
-      this.blue.setVelocityY(-200);
-    } else if (this.blueKeys.s.isDown) {
-      this.blue.setVelocityY(200);
-    } else if (!this.blue.triggered) {
-      this.blue.setVelocity(0);
-    }
-
-    if (this.greenKeys.left.isDown) {
-      this.green.setVelocityX(-200);
-    } else if (this.greenKeys.right.isDown) {
-      this.green.setVelocityX(200);
-    } else if (this.greenKeys.up.isDown) {
-      this.green.setVelocityY(-200);
-    } else if (this.greenKeys.down.isDown) {
-      this.green.setVelocityY(200);
-    } else if (!this.green.triggered) {
-      this.green.setVelocity(0);
-    }
-  }
-}
+import { TestScene } from "../game-engine/scenes/test.scene";
 
 @Component({
   selector: 'app-phaser-test',
@@ -74,7 +17,9 @@ export class PhaserTestComponent implements OnInit {
       type: Phaser.AUTO,
       width: window.innerWidth,
       height: window.innerHeight,
-      scene: [ NewScene ],
+      scene: [
+        TestScene
+      ],
       physics: {
         default: 'arcade',
       },
