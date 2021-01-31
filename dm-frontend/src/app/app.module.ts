9eed557305cb92fab8e0aef9cf724f7d6d2caf60
Md. Hasan-Uz-Zaman Rashid <hz.ce06@gmail.com>
2021-01-31 17:22:54 +0600

    ADD: initial socket io module configuration and angular project support added....this will be used later in the game..

--- dm-frontend/src/app/app.module.ts
+++ dm-frontend/src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PhaserTestComponent } from './phaser-test/phaser-test.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatButtonModule } from "@angular/material/button";
 import { MatTooltipModule } from "@angular/material/tooltip";
 import { MatButtonToggleModule } from "@angular/material/button-toggle";
 import { MatDividerModule } from "@angular/material/divider";
+
+
+const config: SocketIoConfig = { url: 'http://localhost:3000', options: { } };
 
 @NgModule({
   declarations: [
@@ -20,6 +24,7 @@ import { MatDividerModule } from "@angular/material/divider";
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    SocketIoModule.forRoot(config),
     MatToolbarModule,
     MatButtonModule,
     MatTooltipModule,
