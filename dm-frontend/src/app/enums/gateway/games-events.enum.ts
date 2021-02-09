a08c6ecf1b374014d9d534adc462e132d73b573d
Md. Hasan-Uz-Zaman Rashid <hz.ce06@gmail.com>
2021-02-09 16:13:38 +0600

    ADD: enums added taken from backend...must be same for 2 way comm..

---
+++ dm-frontend/src/app/enums/gateway/games-events.enum.ts
@@ -0,0 +1,15 @@
+export enum GamesEventsEnum {
+  ADD_GAME = 'addGame',
+  GET_GAME = 'getGame',
+  GET_GAMES = 'getGames',
+  JOIN_GAME = 'joinGame',
+  LEAVE_GAME = 'leaveGame',
+  CHALLENGE = 'challenge',
+  SET_CHALLENGER = 'setChallenger',
+  SET_FIRST_TO_GO = 'setFirstToGo',
+  INVITER_LEFT = 'inviterLeft',
+  GAMES_LIST = 'gamesList',
+  GAME_INFO = 'gameInfo',
+  USER_INFO = 'userInfo',
+  RESET_GAME = 'resetGame',
+}
