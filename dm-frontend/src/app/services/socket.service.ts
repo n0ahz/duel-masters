eafd3c4fb64deee6a9f4a06bec295dbecf09a769
Md. Hasan-Uz-Zaman Rashid <hz.ce06@gmail.com>
2021-02-09 16:12:11 +0600

    ADD: new service added for managing socket related operations..

---
+++ dm-frontend/src/app/services/socket.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { Socket } from "ngx-socket-io";
+import { SocketPayloadInterface } from "../interfaces/socket-payload.interface";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SocketService {
+
+  constructor(
+    private socket: Socket,
+  ) {
+
+  }
+
+  getCurrentSocket(): Socket {
+    return this.socket;
+  }
+
+  getCurrentSocketId(): string {
+    return this.getCurrentSocket().ioSocket.id;
+  }
+
+  emit(event: string, data?: any) {
+    let socketPayload: SocketPayloadInterface = {};
+    if (data) socketPayload.data = data;
+    this.socket.emit(event, socketPayload);
+  }
+
+  emitTo(gameRoom: string, event: string, data?: any) {
+    let socketPayload: SocketPayloadInterface = {};
+    socketPayload.gameRoom = gameRoom;
+    if (data) socketPayload.data = data;
+    this.socket.emit(event, socketPayload);
+  }
+
+  handleEvent(event: string, callback?: any) {
+    this.getCurrentSocket().on(event, callback);
+  }
+
+  removeAllListeners() {
+    this.getCurrentSocket().removeAllListeners();
+  }
+}
