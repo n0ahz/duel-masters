61e42ca09107bf207ebb9d351ae24a21bc1bd5f3
Md. Hasan-Uz-Zaman Rashid <hz.ce06@gmail.com>
2022-04-03 00:32:32 +0600

    MOD: removed duplicated interfaces from backend.. it is decided now to keep this detail at frontend only...all game data and settings will be at frontend...backend will mainly and only be used for storage and comm...

--- dm-backend/src/interfaces/card.interface.ts
+++ dm-backend/src/interfaces/card.interface.ts
@@ -1,27 +1,21 @@
-import { CivilizationsEnum } from "../enums/civilizations.enum";
-import { RaritiesEnum } from "../enums/rarities.enum";
-import { SetsEnum } from "../enums/sets.enum";
-import { RacesEnum } from "../enums/races.enum";
-
-
 export interface CardInterface {
   name: string;
-  _civilization: CivilizationsEnum;
+  _civilization: string;
   _type: string;
   cost: number;
-  _rarity?: RaritiesEnum;
+  _rarity?: string;
   image?: string;
   manaNumber?: number;
   cardText?: string;
   illustrator?: string;
   flavorText?: string;
-  _set?: SetsEnum;
+  _set?: string;
   cardSerial?: number;
   collectorNo?: string;
   uid?: string;
   // other attributes that are specific to types
   // for Creature
-  race?: RacesEnum;
+  race?: string;
   power?: number;
   powerExtensible?: boolean;
 }
