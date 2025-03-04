95bad579b9d8d9dd675bd86dcb20f11efd34761e
HZ <hz.ce06@gmail.com>
2025-03-04 15:27:57 +0600

    MOD: updating ng version

--- dm-frontend/src/app/guards/auth.guard.ts
+++ dm-frontend/src/app/guards/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { SocialAuthService, SocialUser } from 'angularx-social-login';
 import { map, tap } from 'rxjs/operators';
@@ -8,7 +8,7 @@ import { map, tap } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root',
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard  {
 
   constructor(
     private router: Router,
