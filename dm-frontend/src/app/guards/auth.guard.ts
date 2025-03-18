6232a7f47b2cefcefea7e180b0849d4a40d1896c
HZ <hz.ce06@gmail.com>
2025-03-18 09:36:23 +0600

    FIX: modified import pattern to remove a warning of commonjs dependency

--- dm-frontend/src/app/guards/auth.guard.ts
+++ dm-frontend/src/app/guards/auth.guard.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, map, tap  } from 'rxjs';
 import { SocialAuthService, SocialUser } from '@abacritt/angularx-social-login';
-import { map, tap } from 'rxjs/operators';
 
 
 @Injectable({
